f17685d61f5da50afc9ab9d605784eb1851ddafa
Piotr Kidybiński <piotrkidybinski@gmail.com>
2024-05-09 10:54:13 +0200

    l1

---
+++ lista4/zad1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+class VectorDotProduct {
+private:
+    std::vector<double> vec;
+
+public:
+    
+    VectorDotProduct(const std::vector<double>& v) : vec(v) {}
+
+    
+    double operator*(const std::vector<double>& other) const {
+        double result = 0.0;
+        
+        if (vec.size() != other.size()) {
+            std::cerr << "wektory muyszą mieć tą samą długość" << std::endl;
+            return result;
+        }
+        
+        for (size_t i = 0; i < vec.size(); ++i) {
+            result += vec[i] * other[i];
+        }
+        return result;
+    }
+};
+
+int main() {
+    std::vector<double> v = { 2.0, 2.0, 2.0 };
+    std::vector<double> w = { 3.0, 3.0, 3.0 };
+
+    VectorDotProduct vd_v(v);
+    double dot_product = vd_v * w;
+
+    std::cout << "iloczyn skalarny " << dot_product << std::endl;
+
+    return 0;
+}
---
+++ lista4/zad2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cmath>
+
+double root(double (*f)(double), double a, double b, double tolerance = 1e-10) {
+    double fa = f(a);
+    double fb = f(b);
+
+    if (fa * fb > 0) {
+        std::cerr << "Brak zmiany znaku na zadanym przedziale." << std::endl;
+        return NAN; 
+    }
+
+    double c = 0.0;
+    while (std::abs(b - a) > tolerance) {
+        c = (a + b) / 2;
+        double fc = f(c);
+        if (std::abs(fc) < tolerance) {
+            break;
+        }
+        if (fc * fa < 0) {
+            b = c;
+            fb = fc;
+        }
+        else {
+            a = c;
+            fa = fc;
+        }
+    }
+
+    return c;
+}
+
+double cos_minus_x(double x) {
+    return std::cos(x) - x;
+}
+
+double cos_minus_half(double x) {
+    return std::cos(x) - 0.5;
+}
+
+int main() {
+    double root_1 = root(cos_minus_x, 0, 2, 1e-6);
+    double root_2 = root(cos_minus_half, 0, 1.5, 1e-6);
+
+    std::cout << "Pierwiastek równania cos(x) - x = 0 w przedziale [0, 2] = " << root_1 << std::endl;
+    std::cout << "Pierwiastek równania cos(x) - 0.5 = 0 w przedziale [0, 1.5] = " << root_2 << std::endl;
+
+    return 0;
+}
