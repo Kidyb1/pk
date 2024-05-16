20154397083e8b7779f3f6e4a28bf18fab7d337e
Zbigniew Koza <zbigniew.koza@uwr.edu.pl>
2024-05-16 10:45:02 +0200

    tak to powinno wyglądać

---
+++ lista3/zad2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+struct Link
+{
+    int value;
+    Link* next = nullptr;
+};
+
+void release(Link** phead)
+{
+    if (*phead == nullptr)
+        return;
+
+    Link* tmp = *phead;
+    *phead = (*phead)->next;
+    delete tmp;
+
+    release(phead);
+}
+
+int main()
+{
+    Link* head = new Link{ 1 };
+    head->next = new Link{ 2 };
+    head->next->next = new Link{ 3 };
+
+    release(&head);
+
+    if (head == nullptr)
+        std::cout << "Lista została zwolniona pomyślnie.\n";
+    else
+        std::cout << "Wystąpił błąd podczas zwalniania listy.\n";
+
+    return 0;
+}
